f5c686955850dc833423de000fd0c5860d7f51a2
Kailash Sukumaran <Kailash7dev@gmail.com>
2020-07-28 03:20:15 -0700

    final V

--- app.py
+++ app.py
@@ -1,43 +1,34 @@
 from flask import Flask, render_template, request
-import jsonify
+
 import requests
 import pickle
 import numpy as np
 import sklearn
-from sklearn.preprocessing import StandardScaler
+
 app = Flask(__name__)
 
-# model = pickle.load(open('lin_reg.pkl', 'rb'))
-# model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-with open('lin_reg5.pkl','rb') as f:
+with open('lin_reg6.pkl','rb') as f:
      model = pickle.load(f)
 
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
 
-
-standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 
 def predict():
-    house_Type_condo = 0
-    house_Type_duplex = 0
-    house_Type_house = 0
-    house_Type_townhouse=0
-    house_Type_others=0
-    house_Type_apartment=0
-    parking_type_attached = 0
-    parking_type_detached = 0
-    parking_type_street = 0
-    parking_type_no = 0
+    parking_type_detached = parking_type_attached = house_Type_apartment = house_Type_others = house_Type_townhouse = house_Type_house = house_Type_duplex= house_Type_condo = parking_type_no = parking_type_street = 0
+    
     if request.method == 'POST':
+
         bathroom = float(request.form['bathroom'])
-        area=float(request.form['area'])
+
+        area=np.log(float(request.form['area']))
+
         bedroom=int(request.form['bedroom'])
+
         isFurnished =int(request.form['Finishing'])
-#         Kms_Driven2=np.log(Kms_Driven)
-#         Owner=int(request.form['Owner'])
+
         houseType = request.form['houseType']
         if(houseType=='House'):
             house_Type_house = 1
@@ -53,7 +44,7 @@ def predict():
             house_Type_others = 1
         else:
             house_Type_others = 1
-#         Year=2020-Year
+
         parkingType=request.form['parkingType']
         if(parkingType=='No Parking'):
             parking_type_no = 1
@@ -65,16 +56,12 @@ def predict():
             parking_type_street = 1
         else:
             parking_type_street = 1
-#         Transmission_Mannual=request.form['Transmission_Mannual']
-#         if(Transmission_Mannual=='Mannual'):
-#             Transmission_Mannual=1
-#         else:
-#             Transmission_Mannual=0
+
         x=[bedroom,area,bathroom, isFurnished, house_Type_condo, house_Type_duplex, house_Type_house, house_Type_others,
           house_Type_townhouse, parking_type_detached, parking_type_no, parking_type_street]
-#         x=[2,1232,2, 1, 0, 0, 0, 0,1, 0, 0, 0]
+
         prediction=model.predict([x])
-#         output=round(prediction[0],2)
+
         price = np.exp(prediction)
         price=round(price[0],2)
         if price<0:
--- main.py
+++ main.py
@@ -1,106 +1,77 @@
-<!DOCTYPE html>
-<html lang="en">
+from flask import Flask, render_template, request
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script
-        src="https://code.jquery.com/jquery-3.5.1.slim.js"
-        integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM="
-        crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-</head>
+import requests
+import pickle
+import numpy as np
+import sklearn
 
-<body>
-    <div class="container">
-        <form action="{{ url_for('predict')}}" method="post">
-            <center><h2>Vancouver House Price Estimation</h2></center>
-            <div class="row">
-                <div class="col s12">
-                    <div class="row">
-                    <div class="input-field col s12">
-                        <select id="bedroom" name="bedroom">
-                      	  <option value="" disabled selected>Choose your option</option>
-			  <option value="0">0</option>
-                      	  <option value="1">1</option>
-                      	  <option value="2">2</option>
-                       	  <option value="3">3</option>
-                      	  <option value="4">4</option>
-                      	  <option value="5">5</option>
-                      	  <option value="6">More than 5</option>
-                   	 </select>
-                        <label>Number of Bedrooms</label>
-                    </div>
-                    </div>
-                </div>
-                <div class="col s12">
-                    <div class="row">
-                    <div class="input-field col s12">
-                        <input type="number" id="area" name="area" required>
-                        <label>Area of House</label>
-                    </div>
-                    </div>
-                </div>
-                <div class="col s12">
-                    <div class="row">
-                    <div class="input-field col s12">
-		  	 <select id="bathroom" name="bathroom">
-                      	  <option value="" disabled selected>Choose your option</option>
-			  <option value="1">1</option>
-                      	  <option value="1.5">1.5</option>
-                      	  <option value="2">2</option>
-                       	  <option value="2.5">2.5</option>
-                      	  <option value="3">3</option>
-                      	  <option value="4">More than 3</option>
-                   	 </select>
-                        
-                        <label>Number Of Bathrooms</label>
-                    </div>
-                    </div>
-                </div>
-                <div class="input-field col s12">
-                    <select id="Finishing" name="Finishing">
-                        <option value="" disabled selected>Choose your option</option>
-                        <option value="1">Furnished</option>
-                        <option value="0">Unfurnished</option>
-                    </select>
-                    <label>Furniture</label>
-                </div>
-                <div class="input-field col s12">
-                    <select id="houseType" name="houseType">
-                        <option value="" disabled selected>Choose your option</option>
-                        <option value="House">House</option>
-                        <option value="Condo">Condo</option>
-                        <option value="Apartment">Apartment</option>
-                        <option value="Town House">Town House</option>
-                        <option value="Duplex">Duplex</option>
-			<option value="Others">Others</option>
-                    </select>
-                    <label>House Type</label>
-                </div>
-                <div class="input-field col s12">
-                    <select id="parkingType" name="parkingType">
-                        <option value="" disabled selected>Choose your option</option>
-                        <option value="Attached Garage">Attached Garage</option>
-                        <option value="Detached Garage">Detached Garage</option>
-                        <option value="Street Parking">Street Parking</option>
-                        <option value="No Parking">No Parking</option>
-                    </select>
-                    <label>Parking Type</label>
-                </div>
-            </div>
-            <center><button id="sub" type="submit " class="waves-effect waves-light btn">Calculate the Selling Price</button></center>
-		       <center>{{ prediction_text }}</center>
-    </div>
-        </form>
-    </div>
-    <script>
-        $(document).ready(function(){
-            $('select').formSelect();
-        })
-    </script>
-</body>
+app = Flask(__name__)
 
-</html>
+with open('lin_reg6.pkl','rb') as f:
+     model = pickle.load(f)
+
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+
+def predict():
+    parking_type_detached = parking_type_attached = house_Type_apartment = house_Type_others = house_Type_townhouse = house_Type_house = house_Type_duplex= house_Type_condo = parking_type_no = parking_type_street = 0
+    
+    if request.method == 'POST':
+
+        bathroom = float(request.form['bathroom'])
+
+        area=np.log(float(request.form['area']))
+
+        bedroom=int(request.form['bedroom'])
+
+        isFurnished =int(request.form['Finishing'])
+
+        houseType = request.form['houseType']
+        if(houseType=='House'):
+            house_Type_house = 1
+        elif(houseType=='Condo'):
+            house_Type_condo = 1
+        elif(houseType=='Apartment'):
+            house_Type_apartment = 1
+        elif(houseType=='Town House'):
+            house_Type_townhouse = 1
+        elif(houseType=='Duplex'):
+            house_Type_duplex = 1
+        elif(houseType=='others'):
+            house_Type_others = 1
+        else:
+            house_Type_others = 1
+
+        parkingType=request.form['parkingType']
+        if(parkingType=='No Parking'):
+            parking_type_no = 1
+        elif(parkingType=='Attached Garage'):
+            parking_type_attached = 1
+        elif(parkingType=='Detached Garage'):
+            parking_type_detached = 1
+        elif(parkingType=='Street Parking'):
+            parking_type_street = 1
+        else:
+            parking_type_street = 1
+
+        x=[bedroom,area,bathroom, isFurnished, house_Type_condo, house_Type_duplex, house_Type_house, house_Type_others,
+          house_Type_townhouse, parking_type_detached, parking_type_no, parking_type_street]
+
+        prediction=model.predict([x])
+
+        price = np.exp(prediction)
+        price=round(price[0],2)
+        if price<0:
+            return render_template('index.html',prediction_text="Housing Data provided is not valid")
+        else:
+            return render_template('index.html',prediction_text="You Can Buy/Sell the house at {}".format(price))
+    else:
+         return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
